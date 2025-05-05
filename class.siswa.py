f805935154642d7bcfd7cfdc489590f74c02b8d0
artarmnd <martaramanda279@gmail.com>
2025-05-05 13:13:54 +0700

    materi_minggu4

--- class.siswa.py
+++ class.siswa.py
@@ -2,6 +2,7 @@ class siswa:
     # class variables
     sekolah = 'SMP Negeri 5 kota jambi'
     sekolah2 = 'SD Negeri 41 kota jambi'
+
     # constructor
     def __init__(self, nama, usia, alamat):
         # instance variables
@@ -9,18 +10,25 @@ class siswa:
         self.usia = usia
         self.alamat = alamat
 
-s1 = siswa("Raihan Surya Saputra", 10, "Talang Banjar Jambi")
-print ("=" * 20, "Data Sekolah", 10, "=" * 20)
+
+# input from user
+nama_input = input("Masukkan nama siswa: ")
+usia_input = int(input("Masukkan usia siswa: "))
+alamat_input = input("Masukkan alamat siswa: ")
+
+s1 = siswa(nama_input, usia_input, alamat_input)
+
+print("=" * 20, "Data Sekolah", "=" * 20)
 # access instance variables
-print('Nama Siswa :', s1.nama, "Umur Anak :", "s1.usia, dan Alamat : ",s1.alamat)
+print('Nama Siswa :', s1.nama, "Umur Anak :", s1.usia, "dan Alamat : ", s1.alamat)
 # access class variable
 print('Nama Sekolah :', siswa.sekolah2)
-print ("=" * 25, "Data Sekolah ", "=" * 25)
+print("=" * 25, "Data Sekolah ", "=" * 25)
 
 # Modify instance variables
 s1.nama = 'nazriel irham'
 s1.usia = 43
-s1.alamat="Talang Banjar - Jambi"
-print('nama siswa :', s1.nama, " dan umur anak : ", s1.usia)
+s1.alamat = "Talang Banjar - Jambi"
+print('Nama siswa :', s1.nama, " dan umur anak : ", s1.usia)
 # access class variable
-print('nama sekolah :', siswa.sekolah)
+print('Nama sekolah :', siswa.sekolah)
